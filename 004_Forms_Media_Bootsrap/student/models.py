75493aa7457127e7afb79e97cbf74201660cbb98
yasin yağmur <yasin.yagmur@gmail.com>
2022-08-24 21:21:15 +0300

    fixed

--- 004_Forms_Media_Bootsrap/student/models.py
+++ 004_Forms_Media_Bootsrap/student/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    number = models.IntegerField(blank=True, null=True)
+    number = models.IntegerField(null=True)
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
 
     def __str__(self):
