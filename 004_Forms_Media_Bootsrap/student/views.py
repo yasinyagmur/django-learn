a7219a15859d945d144eb96080a9d51258c4eb37
yasin yağmur <yasin.yagmur@gmail.com>
2022-08-25 16:48:22 +0300

    repeat lesson

--- 004_Forms_Media_Bootsrap/student/views.py
+++ 004_Forms_Media_Bootsrap/student/views.py
@@ -1,10 +1,24 @@
+from multiprocessing import context
 from django.shortcuts import render
+
+
 
 # Create your views here.
 from django.shortcuts import render
+from .forms import StudentForm
+
 
 def index(request):
     return render(request, 'student/index.html')
 
+# def student_page(request):
+#     return render(request,'student/student.html')
+
 def student_page(request):
-    return render(request,'student/student.html')
+    print(request.POST)
+    print(request.FİLES)
+    form = StudentForm()
+    context = {
+        "form": form
+    }
+    return render(request,"student/student.html",context)
