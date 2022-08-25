26afe40d701e5a1d9dc819a7659879679b54d7f8
yasin yağmur <yasin.yagmur@gmail.com>
2022-08-25 16:29:31 +0300

    repeat lesson

--- 004_Forms_Media_Bootsrap/student/views.py
+++ 004_Forms_Media_Bootsrap/student/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'student/index.html')
+
+def student_page(request):
+    return render(request,'student/student.html')
