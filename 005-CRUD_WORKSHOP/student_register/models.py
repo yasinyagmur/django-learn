637ef22ddafef338c0f7fa2f48ad4642e67e763b
yasin yağmur <yasin.yagmur@gmail.com>
2022-08-26 16:39:38 +0300

    crud workshop

--- 005-CRUD_WORKSHOP/student_register/models.py
+++ 005-CRUD_WORKSHOP/student_register/models.py
@@ -1,24 +1,26 @@
 
 # Create your models here.
 from django.db import models
+
+
 class Student(models.Model):
         fullname = models.CharField(max_length=100)
         number = models.CharField(max_length=15)
         mobile = models.CharField(max_length=15)
         email = models.CharField(max_length=50)
         GENDER = (
-        ('Female', "Female"),
-        ('Male', 'Male'),
-        ('Other', 'Other'),
-        ('Prefer', 'Prefer not to say')
+            ('Female', "Female"),
+            ('Male', 'Male'),
+            ('Other', 'Other'),
+            ('Prefer', 'Prefer not to say')
         )
         gender = models.CharField(max_length=50, choices=GENDER)
         PATH = (
-        ('Full Stack', "Full Stack"),
-        ('Data Science', 'Data Science'),
-        ('DevOps', 'DevOps'),
-        ('AWS', 'AWS'),
-        ('ITF', 'ITF')
+            ('Full Stack', "Full Stack"),
+            ('Data Science', 'Data Science'),
+            ('DevOps', 'DevOps'),
+            ('AWS', 'AWS'),
+            ('ITF', 'ITF')
         )
         path = models.CharField(max_length=50, choices=PATH)
 
