dc2516bdb29685da9f30442aff2f5aed8a87c4bf
yasin yağmur <yasin.yagmur@gmail.com>
2022-08-29 21:33:36 +0300

    todo app method post added

--- 007_Todo_App/todo/views.py
+++ 007_Todo_App/todo/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render,redirect
 
 # Create your views here.
-from django.http import HttpResponse
-
+# from django.http import HttpResponse
+from .models import Todo
+from .forms import TodoForm
 
 
 
 def home(request):
-    return HttpResponse("welcome")
+    todos= Todo.objects.all()
+    form = TodoForm()
+    context={
+        "form":form,
+        "todos":todos
+    }
+    return render(request,"todo/home.html",context)
+
+def todo_create(request):
+    form=TodoForm
+    if request.method== "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    context={
+        "form":form
+    }
+    return render(request,"todo/todo_add.html",context)
