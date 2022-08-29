74010aa1c9023fbabea1de7131b9ae07082d894a
yasin yağmur <yasin.yagmur@gmail.com>
2022-08-29 22:26:28 +0300

    todo app update method post added

--- 007_Todo_App/todo/views.py
+++ 007_Todo_App/todo/views.py
@@ -10,6 +10,7 @@ from .forms import TodoForm
 
 def home(request):
     todos= Todo.objects.all()
+    # home kısmında post methodunu yapabilmek için formu context e koyup home.html e gönderdik ve form yapısını home.html e ekledik.
     form = TodoForm()
     context={
         "form":form,
@@ -28,3 +29,18 @@ def todo_create(request):
         "form":form
     }
     return render(request,"todo/todo_add.html",context)
+
+def todo_update(request,id):
+    todo =Todo.objects.get(id=id)
+    form = TodoForm(instance=todo)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context={
+        # "todo":todo,
+        "form":form
+    }
+    return render(request,"todo/todo_update.html",context)
