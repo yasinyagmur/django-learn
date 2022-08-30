541f4912ee182ffe62d231a26b61824d3f6a9bc5
yasin yağmur <yasin.yagmur@gmail.com>
2022-08-30 20:14:26 +0300

    todo class based

--- 007_Todo_App/todo/forms.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import fields
-from socket import fromshare
-from django import forms
-from .models import Todo
-
-class TodoForm(forms.ModelForm):
-    class Meta:
-        model = Todo
-        fields= "__all__"
