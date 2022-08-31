d896bb77aacd71eca9c15c8a9c1bb2077f690767
yasin yağmur <yasin.yagmur@gmail.com>
2022-08-31 22:24:49 +0300

    auth learn for django v3

--- 008_DJ_Auth_1/user_example/views.py
+++ 008_DJ_Auth_1/user_example/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
 
 def home(request):
     return render(request, 'user_example/index.html')
@@ -9,6 +11,24 @@ def special(request):
 
 def register(request):
     form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    #    register olduktan sonra login yapıyor
+    #     username = form.cleaned_data.get("username")
+    #     password = form.cleaned_data.get("password1")
+    #     user = authenticate(username=username, password=password)
+    #     login(request, user)
+    #     return redirect('home')
+    # else:
+    #     form = UserCreationForm()
+    
+    # context = {
+    #     'form': form
+    # }
+    
+    # return render(request, "registration/register.html", context)
+
+        return  redirect("login")
     context = {
         'form': form
     }
