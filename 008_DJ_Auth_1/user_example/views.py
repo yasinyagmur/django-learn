3d486857192bb699a9f542b7171ce88a3529c500
yasin yağmur <yasin.yagmur@gmail.com>
2022-08-31 21:31:23 +0300

    auth learn for django v2

--- 008_DJ_Auth_1/user_example/views.py
+++ 008_DJ_Auth_1/user_example/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request, 'user_example/index.html')
 
 def special(request):
     return render(request, "user_example/special.html")
+
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    return render(request, "registration/register.html", context)
