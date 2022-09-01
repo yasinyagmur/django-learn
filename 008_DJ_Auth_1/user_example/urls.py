bf24f7daa93ea36773cdc9581f7fbfafccda492d
yasin yağmur <yasin.yagmur@gmail.com>
2022-09-01 20:50:49 +0300

    start django auth lesson 2

--- 008_DJ_Auth_1/user_example/urls.py
+++ 008_DJ_Auth_1/user_example/urls.py
@@ -6,10 +6,10 @@ from .views import (
 )
 from django.contrib.auth import views as auth_views
 
-
 urlpatterns = [
     path('', home, name="home"),
-    path('special', special, name="special"),
+    path('special/', special, name="special"),
     path('register/', register, name="register"),
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name="registration/password_change.html"), name="password_change"),
+    path('password_change/', auth_views.PasswordChangeView.as_view(
+        template_name="registration/password_change.html"), name="password_change")
 ]
--- 008_DJ_Auth_1/user_example/views.py
+++ 008_DJ_Auth_1/user_example/views.py
@@ -1,35 +1,36 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, 'user_example/index.html')
 
+
+@login_required
 def special(request):
     return render(request, "user_example/special.html")
 
+
 def register(request):
     form = UserCreationForm(request.POST or None)
+    # form = UserCreationForm()
+    # if request.method == 'POST':
+    #     form = UserCreationForm(request.POST)
     if form.is_valid():
         form.save()
-    #    register olduktan sonra login yapıyor
-    #     username = form.cleaned_data.get("username")
-    #     password = form.cleaned_data.get("password1")
-    #     user = authenticate(username=username, password=password)
-    #     login(request, user)
-    #     return redirect('home')
-    # else:
-    #     form = UserCreationForm()
-    
-    # context = {
-    #     'form': form
-    # }
-    
-    # return render(request, "registration/register.html", context)
 
-        return  redirect("login")
+        # username = form.cleaned_data.get("username")
+        # password = form.cleaned_data.get("password2")
+
+        # user = authenticate(username=username, password=password)
+
+        # login(request, user)
+
+        return redirect("login")
+
     context = {
         'form': form
     }
-    return render(request, "registration/register.html", context)
+    return render(request, 'registration/register.html', context)
